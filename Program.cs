633a935fafa1ba4b73153829c1d4981dc2d036ab
Thiago <thiagodeboliveira@outlook.com>
2024-09-09 00:05:15 -0300

    .NEt_pt2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿    // using dotNet_estudos.Models;
+
+
+    // Pessoa pessoa1 = new Pessoa();
+
+    // pessoa1.Nome = "Thiago";
+    // pessoa1.Idade = 18;
+    // pessoa1.Apresentar();
+
+    DateTime dataAtual1 = DateTime.Now;
+
+    Console.WriteLine(dataAtual1);
