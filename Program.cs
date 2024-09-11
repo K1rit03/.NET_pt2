45104b1581b6534803d12c20a1b1e794a1a3660b
Thiago <thiagodeboliveira@outlook.com>
2024-09-10 23:50:32 -0300

    praticando for

--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
 ﻿    
     using dotNet_estudos.Models;
    
+int numero = 5;
+int contador = 0;
+while (contador <= 10){
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    int numero = 5;
+   
+   
+//    for(int contador = 0; contador <= 10; contador++){
+//         Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//    }
    
    
    
@@ -12,9 +43,17 @@
    
    
    
-   
-   
-     Calculadora calc = new Calculadora();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //  Calculadora calc = new Calculadora();
 
     // calc.Somar(10,5);
     // calc.Dividir(20,4);
@@ -34,7 +73,7 @@
     // Console.WriteLine("Decrementando o 10");
     // numero2 --;
     // Console.WriteLine(numero2);
-    calc.RaizQuadrada(9);
+    // calc.RaizQuadrada(9);
 
 
 
--- obj/Debug/net8.0/dotNet_estudos.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotNet_estudos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotNet_estudos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b90a831f3aaa65e5269320a9f6b6ed521ecfa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aec9a9553f6f0cac2ec202e07373541cfe38b10")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotNet_estudos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotNet_estudos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
